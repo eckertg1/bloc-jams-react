f32100b4ce55cbd3813d0712eabf0fadb0fbda87
eckertg1 <eckertg1@yahoo.com>
2018-04-18 03:06:03 -0400

    checkpoint 7 complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,7 +9,39 @@ class Album extends Component {
       return album.slug === this.props.match.params.slug
     });
 
-    this.state={album : album};
+    this.state={
+                album : album,
+                currentSong: album.songs[0],
+                isPlaying: false
+              };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true});
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false});
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song});
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    }
+    else{
+      this.play();
+    }
   }
 
   render() {
@@ -30,11 +62,17 @@ class Album extends Component {
             <col id="song-duration-column"/>
           </colgroup>
           <tbody>
-            {this.state.album.songs.map( (songs,index) =>
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{songs.title}</td>
-                <td>{songs.duration}</td>
+            {this.state.album.songs.map( (song,index) =>
+              <tr key={index} className="song" onClick={() => this.handleSongClick(song)}>
+                <td className="song-actions">
+                  <button>
+                    <span className="song-number">{index + 1 }</span>
+                    <span className="ion-play"></span>
+                    <span className="ion-pause"></span>
+                  </button>
+                </td>
+                <td className="song-title">{song.title}</td>
+                <td className="song-duration">{song.duration}</td>
               </tr>
               )
             }
